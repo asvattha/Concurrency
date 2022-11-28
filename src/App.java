3d06fa51a31fa9fdb1e7dd6043386b0120349e7e
singhd1 <singhd1@sfasu.edu>
2022-11-28 10:52:32 -0600

    Thread interruption Example

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App implements Runnable{
 
+    static boolean isInterrupted = false;
+    static Object myLock = new Object();
 
     @Override
     public void run() {
@@ -9,9 +11,14 @@ public class App implements Runnable{
     public static void foo() {
         for (int i = 0; i < 10; i++){
             try{
+                if (isInterrupted){
+                    System.out.println("Thread stopped");
+                    return;
+                }
                 Thread.sleep(1000);
             } catch(InterruptedException e) {
-
+                System.out.println("I am not finished yet!!");
+                return;
             }
             
             System.out.printf("%s %s%n", "*", Thread.currentThread().getName());
@@ -23,6 +30,8 @@ public class App implements Runnable{
         t.setName("Thread 1");
         t.start();
         t.join(5000);
+        //t.interrupt(); // interrupting it forcefully
+        isInterrupted = true;
 
         System.out.printf("%s is finished%n", Thread.currentThread().getName());
     }
